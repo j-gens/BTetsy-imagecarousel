448b9fb53b4227d9d647b5572fe31d7ecd4b2fd7
j-gens <jgens2@gmail.com>
2019-11-09 09:12:59 -0800

    Update routes to use postgres queries

--- server/database/index.js
+++ server/database/index.js
@@ -12,19 +12,28 @@ const pool = new Pool({
 
 module.exports = {
   getProductQuery: (productId, callback) => {
-    return pool.query(`SELECT * FROM products INNER JOIN product_images ON products.productId = product_images.productId WHERE products.productId = ?;`, productId, callback)
+    return pool.query(`SELECT * FROM products INNER JOIN product_images ON products.productId = product_images.productId WHERE products.productId = $1`, productId, (err, data) => {
+      callback(err, data)
+    })
   },
 
   updateIsLikedQuery: (productId, callback) => {
-    return pool.query(`UPDATE products SET isLiked = NOT isLiked WHERE productId = ?;`, productId, callback)
+    return pool.query(`UPDATE products SET isLiked = NOT isLiked WHERE productId = $1`, productId, (err, data) => {
+      callback(err, data)
+    })
   },
 
   removeProductQuery: (productId, callback) => {
-    return pool.query(`DELETE FROM products WHERE productId = ?`, productId, callback)
-  }
+    return pool.query(`DELETE FROM products WHERE productId = $1`, productId, (err, data) => {
+      callback(err, data)
+    })
+  },
 
   addProductQuery: (params, callback) => {
-    return pool.query(`INSERT INTO products (productId, productName, isLiked) VALUES (?, ?, ?)`, params, callback)
+    return pool.query(`INSERT INTO products (productId, productName, isLiked) VALUES ($1, $2, $3)`, params, (err, data) => {
+      console.log(err, data)
+      callback(err, data)
+    })
   },
 }
 
